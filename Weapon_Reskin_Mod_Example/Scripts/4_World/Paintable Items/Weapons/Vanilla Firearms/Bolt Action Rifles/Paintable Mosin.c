65012c99801bbd7b41174d3c76ff0b781fcf5fb7
Dumpgrah <Andrewthompson0200@gmail.com>
2022-09-09 12:00:53 -0700

    just more fixes and spacing

--- Weapon_Reskin_Mod_Example/Scripts/4_World/Paintable Items/Weapons/Vanilla Firearms/Bolt Action Rifles/Paintable Mosin.c
+++ Weapon_Reskin_Mod_Example/Scripts/4_World/Paintable Items/Weapons/Vanilla Firearms/Bolt Action Rifles/Paintable Mosin.c
@@ -3,9 +3,13 @@ modded class Mosin9130_Base
 	override void ND_InitTextures()
 	{
 		super.ND_InitTextures();
+									// Name, //Color///////////////////////////////////////////////////////////// Rvmat////////////////////////////////////////////////////
 		ND_RegisterTextureAndMaterial("camo", "DZ\\weapons\\firearms\\mosin9130\\data\\mosin_nagant_camo_co.paa","dz\\weapons\\firearms\\mosin9130\\data\\mosin_9130.rvmat");
+		
 		ND_RegisterTextureAndMaterial("black", "#(argb,8,8,3)color(0.0,0.0,0.0,1.0,CO)","dz\\weapons\\firearms\\mosin9130\\data\\mosin_9130_bk.rvmat");
+		
 		ND_RegisterTextureAndMaterial("Strip", "dz\\weapons\\firearms\\mosin9130\\data\\mosin_9130_co.paa","dz\\weapons\\firearms\\mosin9130\\data\\mosin_9130.rvmat");
+		
 	}
 	
 	//override void EEOnCECreate()
